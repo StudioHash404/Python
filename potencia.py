fc9944fd88a622eaecebc49acd59cf61db6c16a3
Memoryleak <182170131+StudioHash404@users.noreply.github.com>
2025-04-18 13:09:07 -0300

    Meu primeiro programa em Python!
    Potenciação matemática em Python

---
+++ potencia.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite seu primeiro numero: "));
+
+num2 = int(input("Digite seu segundo numero: "));
+
+resultado = num1 ** num2;
+
+print("O resultado de", num1, "e", num2, "em potência, é: ", resultado);
